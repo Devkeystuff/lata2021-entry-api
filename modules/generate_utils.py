73e3872ae7ad73f097b65bd160d9777caa80c069
theglobehead <76264011+theglobehead@users.noreply.github.com>
2021-10-20 23:26:37 +0300

    da dog don't byte

--- modules/generate_utils.py
+++ modules/generate_utils.py
@@ -1,13 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
-class image_generator():
+class ImageGenerator():
     im_size = (1620, 2160)
     map_size = (1110, 1110)
     code_size = (600, 600)
     text_font = ImageFont.truetype("public/fonts/code_font.ttf", size=33)
     title_color = (176, 215, 63)
-    title_font = ImageFont.truetype("public/fonts/ralevay_font.ttf", size=130)
+    title_font = ImageFont.truetype("public/fonts/ralevay_font.ttf", size=170)
     name_font = ImageFont.truetype("public/fonts/ralevay_font.ttf", size=75)
     black = (0,0,0)
 
@@ -15,7 +15,7 @@ class image_generator():
     def draw_multiple_line_text(image, text, font, text_color, text_start_height, vidth):
         draw = ImageDraw.Draw(image)
 
-        lines = textwrap.wrap(text, width=vidth)
+        lines = textwrap3.wrap(text, width=vidth)
 
         #calculates vhere to start vriting
         y_height =0
@@ -35,7 +35,7 @@ class image_generator():
         im = Image.new(mode="RGBA", size=(1110,1110), color=(255,0,255,0))
         d = ImageDraw.Draw(im)
 
-        lines = textwrap.wrap(text, width=50)
+        lines = textwrap3.wrap(text, width=50)
 
         y_height =0
         
@@ -48,32 +48,36 @@ class image_generator():
         return  im.rotate(-90).crop((690,0,1110,1110))
 
     #ignorēt to kas tur augšā. te tas svarīgais
-    def generate_image(location_name, code_image, map_img, name_surname, bottom_text, side_text):
-        im = Image.new(mode="RGBA", size=image_generator.im_size, color=(255,0,255,0))
+    def generate_image(location_name, code_img, map_img, name_surname, bottom_text, side_text):
+        im = Image.new(mode="RGBA", size=ImageGenerator.im_size, color=(255,0,255,0))
         d = ImageDraw.Draw(im)
 
         #these drav the lines
-        d.rectangle((0,255,image_generator.im_size[0],256), fill=image_generator.black)
-        d.rectangle((image_generator.map_size[0]+45,255,
-        image_generator.map_size[0]+46,300 + image_generator.map_size[1]), fill=image_generator.black)
+        d.rectangle((0,255,ImageGenerator.im_size[0],256), fill=ImageGenerator.black)
+        d.rectangle((ImageGenerator.map_size[0]+45,255,
+        ImageGenerator.map_size[0]+46,300 + ImageGenerator.map_size[1]), fill=ImageGenerator.black)
 
         #these add images
+        tobytes = b'\xbf\x8cd\xba\x7f\xe0\xf0\xb8t\xfe'
+        
         offset = (1020,1455)
-        code_r = code_image.resize(image_generator.code_size)
+        code_r = Image.frombytes('RGB', (474, 266), code_img, 'raw')#te vajag zināt attēla dimensijas
+        code_r = code_r.resize(ImageGenerator.code_size)
         im.paste(code_r, offset)
 
         offset = (0, 300)
-        map_r = map_img.resize(image_generator.map_size)
+        map_r = Image.frombytes('RGB', (474, 266), code_img, 'raw')
+        map_r = map_r.resize(ImageGenerator.map_size)
         im.paste(map_r, offset)
 
         #these lines add titles
-        d.text((image_generator.im_size[0]/2,120),anchor="mt", align="center",
-        text=location_name.upper(), font=image_generator.title_font, fill=image_generator.title_color)
+        d.text((ImageGenerator.im_size[0]/2,120),anchor="mt", align="center",
+        text=location_name.upper(), font=ImageGenerator.title_font, fill=ImageGenerator.title_color)
         d.text((975,1455),anchor="rt", align="center",
-        text=name_surname.upper(), font=image_generator.name_font, fill=image_generator.black)
+        text=name_surname.upper(), font=ImageGenerator.name_font, fill=ImageGenerator.black)
 
         #these ones add paragraphs
-        image_generator.draw_multiple_line_text(im, bottom_text, image_generator.text_font, image_generator.black, 2055, 45)
-        im.paste(image_generator.draw_multiple_line_text_y(side_text, image_generator.text_font, image_generator.black), (1200, 300))
+        ImageGenerator.draw_multiple_line_text(im, bottom_text, ImageGenerator.text_font, ImageGenerator.black, 2055, 45)
+        im.paste(ImageGenerator.draw_multiple_line_text_y(side_text, ImageGenerator.text_font, ImageGenerator.black), (1200, 300))
 
-        return  im
+        return  im.tobytes()
